6498441352776fcfd2444275aae8df202de9daf1
Anton <antonstendal19@gmail.com>
2017-12-19 16:35:05 +0200

    Second commit

--- Script/script.js
+++ Script/script.js
@@ -71,95 +71,93 @@ function close1() {
 }
 
 function close2 () {
- var closeG = document.getElementById("open_window2");
- closeG.style.display = "none";
+	var closeG = document.getElementById("open_window2");
+	closeG.style.display = "none";
 }
 
 
  
-function acceptance(newsArr) {
-              for(key in newsArr) {
-                var title = newsArr[key].title;
-                var author = newsArr[key].author;
-                var description = newsArr[key].description;
-                var publishedAt = newsArr[key].publishedAt;
-                var url = newsArr[key].url;
-                var urlToImage = newsArr[key].urlToImage;
-                var createNews = document.getElementById('parse_news');
+	function acceptance(newsArr) {
+        for(key in newsArr) {
+        var title = newsArr[key].title;
+        var author = newsArr[key].author;
+        var description = newsArr[key].description;
+        var publishedAt = newsArr[key].publishedAt;
+        var url = newsArr[key].url;
+        var urlToImage = newsArr[key].urlToImage;
+        var createNews = document.getElementById('parse_news');
 
-                var newsItem = document.createElement('div');
-                newsItem.className = 'news-item';
-                createNews.appendChild(newsItem);
+        var newsItem = document.createElement('div');
+        newsItem.className = 'news-item';
+        createNews.appendChild(newsItem);
 
-                var newsTitle = document.createElement('h3');
-                newsTitle.className = 'news-title';
-                newsItem.appendChild(newsTitle);
+        var newsTitle = document.createElement('h3');
+        newsTitle.className = 'news-title';
+        newsItem.appendChild(newsTitle);
 
-                var aTitle = document.createElement('a');
-                aTitle.href = url;
-                aTitle.textContent = title;
-                newsTitle.appendChild(aTitle);
+        var aTitle = document.createElement('a');
+        aTitle.href = url;
+        aTitle.textContent = title;
+        newsTitle.appendChild(aTitle);
 
-                var div = document.createElement('div');
-                newsItem.appendChild(div);
+        var div = document.createElement('div');
+        newsItem.appendChild(div);
 
-                var newsRubric = document.createElement('span');
-                newsRubric.className = 'news-rubric';
-                newsRubric.textContent = author;
-                div.appendChild(newsRubric);
+        var newsRubric = document.createElement('span');
+        newsRubric.className = 'news-rubric';
+        newsRubric.textContent = author;
+        div.appendChild(newsRubric);
 
-                var newsData = document.createElement('span');
-                newsData.className = 'news-data';
-                newsData.textContent = publishedAt;
-                div.appendChild(newsData);
+        var newsData = document.createElement('span');
+        newsData.className = 'news-data';
+        newsData.textContent = publishedAt;
+        div.appendChild(newsData);
 
-                var clearfix = document.createElement('div');
-                clearfix.className = 'clearfix';
-                newsItem.appendChild(clearfix);
+        var clearfix = document.createElement('div');
+        clearfix.className = 'clearfix';
+        newsItem.appendChild(clearfix);
 
-                var aContent = document.createElement('a');
-                aContent.className = 'col-xs-12 col-sm-5 col-md-4 news-img';
-                aContent.href = url;
-                clearfix.appendChild(aContent);
+        var aContent = document.createElement('a');
+        aContent.className = 'col-xs-12 col-sm-5 col-md-4 news-img';
+        aContent.href = url;
+        clearfix.appendChild(aContent);
 
-                var imgContent = document.createElement('img');
-                imgContent.src = urlToImage;
-                imgContent.alt = title;
-                imgContent.style.width = '100%';
-                aContent.appendChild(imgContent);
+        var imgContent = document.createElement('img');
+        imgContent.src = urlToImage;
+        imgContent.alt = title;
+        imgContent.style.width = '100%';
+        aContent.appendChild(imgContent);
 
-                var newsText = document.createElement('div');
-                newsText.className = 'col-xs-12 col-sm-7 col-md-4 news-text';
-                newsText.textContent = description;
-                clearfix.appendChild(newsText);
+        var newsText = document.createElement('div');
+        newsText.className = 'col-xs-12 col-sm-7 col-md-4 news-text';
+        newsText.textContent = description;
+        clearfix.appendChild(newsText);
               }
             }
 
          
-          $.get("https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=6f7c85381a5c44deb7e024cd02c60e31",
-            function(e) {
-              var newsArr = e.articles;
-              acceptance(newsArr);
+    $.get("https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=6f7c85381a5c44deb7e024cd02c60e31",
+        function(e) {
+            var newsArr = e.articles;
+            acceptance(newsArr);
             }
           );
 
 		window.addEventListener("DOMContentLoaded", function (){
 			
 		var harm = document.getElementsByClassName ('harmonic')
-		for (var i = 0; i < harm.length; i++){
-			console.log (harm[i]);
+			for (var i = 0; i < harm.length; i++){
 			harm[i].addEventListener ("click", function (element) {
-				document.querySelector('harmonic')
-			console.log (element.target);
+			document.querySelector('harmonic')
+			
+		var list = element.target.querySelector("ul")	
+			if (list.style.display ==="block") { 
+				list.style.display = "none";
+				} 
+			else {
+				list.style.display = "block";
+				}		
+					
 			})
-			
 		}
-			
-			
-		
-		
-		
-	
-	
-	
 	})
